3cd34c909c4c996c3ea9863d2a49998fe8588678
CWass09 <cwassarman@gmail.com>
2025-08-28 23:21:45 -0500

    delete

--- assets/js/site.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelectorAll('.pill, .plan').forEach(el=>{
-  el.addEventListener('mousemove', e=>{
-    const r = el.getBoundingClientRect();
-    el.style.setProperty('--mx', (e.clientX - r.left) + 'px');
-    el.style.setProperty('--my', (e.clientY - r.top) + 'px');
-  });
-});
